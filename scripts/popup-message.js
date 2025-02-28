632e63d1105ea8dc8a108341a6705cc6e4f06dcd
idklol <102387929+RbgPro4@users.noreply.github.com>
2025-02-28 09:59:47 +0200

    Delete scripts/popup-message.js

--- scripts/popup-message.js
+++
@@ -1,5 +0,0 @@
-// Close the popup when the user clicks the button
-document.getElementById('closePopup').addEventListener('click', function() {
-    document.getElementById('popup').style.visibility = 'hidden';
-    document.getElementById('popup').style.opacity = '0';
-});

e2c47609e70c4c96e6cc89ffc7ee5efa3f1000d6
idklol <102387929+RbgPro4@users.noreply.github.com>
2025-02-28 09:46:23 +0200

    Add files via upload

---
+++ scripts/popup-message.js
@@ -0,0 +1,5 @@
+// Close the popup when the user clicks the button
+document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('popup').style.visibility = 'hidden';
+    document.getElementById('popup').style.opacity = '0';
+});
